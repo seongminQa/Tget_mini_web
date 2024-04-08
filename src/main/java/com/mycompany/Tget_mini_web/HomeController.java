4b3de27181a682bac81b47ab2d383b398b283eff
seongminQa <znznfltm@gmail.com>
2024-04-09 01:21:07 +0900

    2024 - 04 - 08 (월) 1차 미니 프로젝트 점검 및 발표 home페이지 구성

--- src/main/java/com/mycompany/Tget_mini_web/HomeController.java
+++ src/main/java/com/mycompany/Tget_mini_web/HomeController.java
@@ -3,16 +3,15 @@ package com.mycompany.Tget_mini_web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import jdk.internal.org.jline.utils.Log;
+//import jdk.internal.org.jline.utils.Log;
 import lombok.extern.slf4j.Slf4j;
 
-@Controller
 @Slf4j
+@Controller
 @RequestMapping("/")
 public class HomeController {
 	@RequestMapping("")
 	public String index() {
-		Log.info("index() ½ÇÇà"); 
 		return "home";
 	}
 }
