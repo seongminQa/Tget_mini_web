6a9cb11f7c19f670d10924c2ffcd10fce5e15958
이재원 <ljwryan@gmail.com>
2024-04-16 21:39:40 +0900

    재원-itemcontroller 생성

---
+++ src/main/java/com/mycompany/Tget_mini_web/controller/ItemController.java
@@ -0,0 +1,17 @@
+package com.mycompany.Tget_mini_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@Controller
+@RequestMapping("/item")
+public class ItemController {
+	@RequestMapping("")
+	public String detail() {
+		log.info("detail()실행");
+		return "item/detail";
+	}
+
+}
