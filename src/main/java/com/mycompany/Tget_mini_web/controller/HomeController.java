7142052fc0860a33a664675eb3a3556ada804fd6
이재원 <ljwryan@gmail.com>
2024-04-16 21:39:23 +0900

    재원-homecontroller 생성

---
+++ src/main/java/com/mycompany/Tget_mini_web/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.mycompany.Tget_mini_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//import jdk.internal.org.jline.utils.Log;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	@RequestMapping("")
+	public String index() {
+		return "home";
+	}
+}
