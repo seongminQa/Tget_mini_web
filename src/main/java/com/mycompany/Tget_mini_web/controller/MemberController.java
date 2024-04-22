7ba3c287441a8fb46be1b853b90f522d9d4a16f1
ryan <ryan@Karen>
2024-04-22 22:39:49 +0900

    재원-membercontroller(아이디찾기,비번찾기,이용후기,구매내역)작성

--- src/main/java/com/mycompany/Tget_mini_web/controller/MemberController.java
+++ src/main/java/com/mycompany/Tget_mini_web/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.mycompany.Tget_mini_web.controller;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,22 +22,54 @@ public class MemberController {
 		log.info("member.index() 실행");
 		return "member/index";
 	}
+
 	// member 로그인 페이지 매핑
 	@RequestMapping("/login")
 	public String login() {
 		log.info("member.login() 실행");
 		return "member/log_in";
 	}
+
 	// member 회원가입 페이지 매핑
 	@RequestMapping("/sign_up")
 	public String sign_up() {
 		log.info("member.sign_up() 실행");
 		return "member/sign_up";
 	}
+
 	// member 장바구니 페이지 매핑
 	@RequestMapping("/shoppingcart")
 	public String shoppingCart() {
 		log.info("member.shopping_cart() 실행");
 		return "member/shopping_cart";
 	}
+
+	// 리뷰
+	@RequestMapping("/review_ing")
+	public String review_ing() {
+		log.info("member.review_ing() 실행");
+		return "member/review_ing";
+	}
+
+	// 구매내역
+	@RequestMapping("/purchase")
+	public String purchase() {
+		log.info("member.purchase() 실행");
+		return "member/purchase";
+	}
+
+	// 아이디 찾기
+	@RequestMapping("/lost_id")
+	public String lost_id() {
+		log.info("member.lost_id() 실행");
+		return "member/lost_id";
+	}
+
+	// 비밀번호 찾기
+	@RequestMapping("/lost_pw")
+	public String lost_pw() {
+		log.info("member.lost_pw() 실행");
+		return "member/lost_pw";
+	}
+
 }
