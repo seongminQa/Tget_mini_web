6ec6d1c628adb9253768e204ed4b4517b1837313
seongminQa <znznfltm@gmail.com>
2024-04-26 18:39:04 +0900

    성민 : ERD를 DTO에 객체화 총 10개

---
+++ src/main/java/com/mycompany/Tget_mini_web/dto/CartDetail.java
@@ -0,0 +1,7 @@
+package com.mycompany.Tget_mini_web.dto;
+
+public class CartDetail {
+	private int cno;
+	private int pno;
+	private int camount;
+}
---
+++ src/main/java/com/mycompany/Tget_mini_web/dto/Product.java
@@ -0,0 +1,23 @@
+package com.mycompany.Tget_mini_web.dto;
+
+import java.sql.Blob;
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Product {
+	private int pno;
+	private String pkind;
+	private String ptitle;
+	private String pplace;
+	private int pprice;
+	private Blob pcontent;
+	private Blob pposter;
+	private Blob pactors;
+	private Date pdateStart;
+	private Date pdateEnd;
+	private String pseatgrade;
+	private int pseatcnt;
+	private String paddress;
+}
