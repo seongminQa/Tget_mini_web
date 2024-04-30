e588ff4e97e59cfec40f09267e4dcae90a288b78
seongminQa <znznfltm@gmail.com>
2024-04-30 20:14:11 +0900

    성민: 컨트롤러 생성

---
+++ src/main/java/com/mycompany/Tget_mini_web/controller/Tdb.java
@@ -0,0 +1,13 @@
+package com.mycompany.Tget_mini_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/tdb")
+public class Tdb {
+	
+}
