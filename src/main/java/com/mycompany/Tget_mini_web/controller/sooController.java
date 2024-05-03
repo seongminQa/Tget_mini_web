a2780db946542aa3f3d93b66df68d0538970a0a8
soo <hongmoon0430@gmail.com>
2024-05-03 21:01:11 +0900

    수민 - test용 Controller 생성

---
+++ src/main/java/com/mycompany/Tget_mini_web/controller/sooController.java
@@ -0,0 +1,24 @@
+package com.mycompany.Tget_mini_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/payment")
+public class sooController {
+	
+	@GetMapping("/payment_cancel")
+	public String payment_cancel() {
+		return "payment/payment_cancel";
+	}
+	
+	@GetMapping("/payment_completed")
+	public String payment_complement() {
+		return "payment/payment_completed";
+	}
+
+}
