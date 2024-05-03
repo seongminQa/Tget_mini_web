270a3ba95eb78e29c0c8f672b0919878a00ce7d7
seongminQa <znznfltm@gmail.com>
2024-05-03 21:28:10 +0900

    성민 : 유저 insert 더미 파일 코드 추가

--- src/main/java/com/mycompany/Tget_mini_web/controller/AdminController.java
+++ src/main/java/com/mycompany/Tget_mini_web/controller/AdminController.java
@@ -1,8 +1,19 @@
 package com.mycompany.Tget_mini_web.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.Tget_mini_web.dao.MemberDao;
+import com.mycompany.Tget_mini_web.dto.MemberDto;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,6 +27,9 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	@Autowired
+	private MemberDao memberDao;
+	
 	@RequestMapping("")
 	public String index() {
 		log.info("admin.index() 실행");
@@ -50,12 +64,44 @@ public class AdminController {
 		return "/admin/member/m_read";
 	}
 	
-	@RequestMapping("/memberSignup")
-	public String memberSignup(Model model) {
-		log.info("admin.memberSignup() 실행");
-		model.addAttribute("admin", "member");
-		return "/admin/member/m_signup";
+	// 회원 등록 해보기
+	@GetMapping("/signupMemberTest")
+	public String signupMember() throws Exception {
+		/*LocalDate now = LocalDate.now();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		String formatedNow = now.format(dtf);
+		
+        // 문자열        
+		String dateStr = formatedNow;         
+		// 포맷터        
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");         
+		// 문자열 -> Date        
+		Date date = formatter.parse(dateStr);*/
+		
+		String date="20240503";
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+
+		for(int i=2; i<25; i++) {
+			MemberDto memberDto = new MemberDto();
+			memberDto.setMid("user" + Integer.toString(i));
+			memberDto.setMpassword("{noop}" + Integer.toString(12345));
+			memberDto.setMname("유저" + i);
+			memberDto.setMssn(Integer.toString(123456)); // mssn char(30)으로 변경하기
+			memberDto.setMemail("seongmin@gmail.com");
+			memberDto.setMaddress("서울시 강동구");
+			memberDto.setMphone("010-1234-1234");
+			memberDto.setMnickname("line8");
+			memberDto.setMsince(format.parse(date));
+			memberDto.setMprefergenre("코믹");
+			memberDto.setMgrade("Gold");
+			//memberDto.setMprofileImg(null); // 프로필 이미지 생략
+			memberDto.setMtotalpay(i*10000);
+			memberDto.setMrole("유저");
+			memberDao.insert(memberDto);
+		}
+		return "redirect:/";
 	}
+	
 	
 	@RequestMapping("/memberModify")
 	public String memberModify(Model model) {
