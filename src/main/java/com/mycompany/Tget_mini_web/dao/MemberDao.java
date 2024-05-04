92ec17b206c288a313579716cfc9887d0a6e5dce
seongminQa <znznfltm@gmail.com>
2024-05-04 19:36:27 +0900

    성민 : 페이징 재정의 메소드 선언

--- src/main/java/com/mycompany/Tget_mini_web/dao/MemberDao.java
+++ src/main/java/com/mycompany/Tget_mini_web/dao/MemberDao.java
@@ -1,11 +1,16 @@
 package com.mycompany.Tget_mini_web.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.mycompany.Tget_mini_web.dto.MemberDto;
+import com.mycompany.Tget_mini_web.dto.PagerDto;
 
 @Mapper
 public interface MemberDao {
 	MemberDto selectByMid(String username);
-	MemberDto insert(MemberDto memberdto);
+	public int insert(MemberDto memberDto);
+	List<MemberDto> selectByPage(PagerDto pager);
+	int count();
 }
