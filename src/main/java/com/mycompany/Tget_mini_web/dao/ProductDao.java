c5bb3a360a147af2773df400e1d007d563a0c8c8
seongminQa <znznfltm@gmail.com>
2024-05-06 22:36:05 +0900

    성민 : ProductDao 추상 메소드 추가 // 작업 중

--- src/main/java/com/mycompany/Tget_mini_web/dao/ProductDao.java
+++ src/main/java/com/mycompany/Tget_mini_web/dao/ProductDao.java
@@ -1,8 +1,19 @@
 package com.mycompany.Tget_mini_web.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.mycompany.Tget_mini_web.dto.PagerDto;
+import com.mycompany.Tget_mini_web.dto.ProductDto;
 
 @Mapper
 public interface ProductDao {
-
+	public int insertProduct(ProductDto productDto);
+	public List<ProductDto> selectByPage(PagerDto pager);
+	public int count();
+	public int updateProduct(ProductDto productDto);
+	public void deleteByPno(int pno);
+	public ProductDto selectByPno(int pno);
+	public int deleteByMid(int pno);
 }
