b4fa92c5f382e527234d9e6ccec18c5152a3500c
김명환 <myeonghwan57@gmail.com>
2024-05-07 16:49:38 +0900

    명환: BoardDto.java 클래스 파일 작성

--- src/main/java/com/mycompany/Tget_mini_web/dto/BoardDto.java
+++ src/main/java/com/mycompany/Tget_mini_web/dto/BoardDto.java
@@ -2,17 +2,21 @@ package com.mycompany.Tget_mini_web.dto;
 
 import java.util.Date;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 
 @Data
 public class BoardDto {
 	private int bno;
-	private String bimg;
 	private String btitle;
 	private String bcontent;
 	private String mid;
 	private Date bdate;
 	private int bhitcount;
 	
-
+	private MultipartFile bimg;
+	private byte[] bimgdata;
+	
+	
 }
