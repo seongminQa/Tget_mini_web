3c930c73ba0250f0ad27d9d3d647cb5e6db30187
김명환 <myeonghwan57@gmail.com>
2024-05-07 16:50:24 +0900

    명환 - BoardDao.java 파일 생성 (insert 작성)

--- src/main/java/com/mycompany/Tget_mini_web/dao/BoardDao.java
+++ src/main/java/com/mycompany/Tget_mini_web/dao/BoardDao.java
@@ -2,7 +2,11 @@ package com.mycompany.Tget_mini_web.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.mycompany.Tget_mini_web.dto.BoardDto;
+
 @Mapper
 public interface BoardDao {
+
+	public int insert(BoardDto boardDto);
 
 }
