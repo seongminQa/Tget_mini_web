bdec29886c0227658e1d583cd2463741294ce302
김명환 <myeonghwan57@gmail.com>
2024-05-07 18:16:22 +0900

    명환 - BoardController.java 파일 추가 작성(boardList)

--- src/main/java/com/mycompany/Tget_mini_web/controller/BoardController.java
+++ src/main/java/com/mycompany/Tget_mini_web/controller/BoardController.java
@@ -1,12 +1,20 @@
 package com.mycompany.Tget_mini_web.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mycompany.Tget_mini_web.dto.BoardDto;
+import com.mycompany.Tget_mini_web.dto.PagerDto;
 import com.mycompany.Tget_mini_web.service.BoardService;
+
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -50,4 +58,33 @@ public class BoardController {
 
 		return "redirect:/board";
 	}
+
+	@GetMapping("")
+	public String boardList(String pageNo, Model model, HttpSession session) {
+
+		if (pageNo == null) {
+			pageNo = (String) session.getAttribute("pageNo");
+			// 세션에 저장되어 있지 않을 경우"1"로 강제 세팅
+			if (pageNo == null) {
+				pageNo = "1";
+			}
+		}
+		// 세션에 pageNo 저장
+		session.setAttribute("pageNo", pageNo);
+		// 문자열을 정수로 반환
+		int intPageNo = Integer.parseInt(pageNo);
+
+		// Pager 객체 생성
+		int rowsPagingTarget = service.getTotalRows();
+		PagerDto pagerDto = new PagerDto(10, 10, rowsPagingTarget, intPageNo);
+
+		// Service에서 게시물 목록 요청
+		List<BoardDto> boardList = service.getBoardList(pagerDto);
+
+		model.addAttribute("pagerDto",  pagerDto);
+		model.addAttribute("boardList", boardList);
+		return "board/index";
+
+	}
+
 }
