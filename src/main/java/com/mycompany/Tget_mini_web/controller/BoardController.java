d8df3ebf78230cede3016271e9587c9b20d0c106
김명환 <myeonghwan57@gmail.com>
2024-05-07 16:51:46 +0900

    명환 - BoardController.java 게시글 작성 DB 연동 완료

--- src/main/java/com/mycompany/Tget_mini_web/controller/BoardController.java
+++ src/main/java/com/mycompany/Tget_mini_web/controller/BoardController.java
@@ -1,7 +1,12 @@
 package com.mycompany.Tget_mini_web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.Tget_mini_web.dto.BoardDto;
+import com.mycompany.Tget_mini_web.service.BoardService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,9 +21,33 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+	@Autowired
+	private BoardService service;
+
 	@RequestMapping("")
 	public String index() {
 		log.info("board.index() 실행");
 		return "board/index";
+	}
+
+	@RequestMapping("writeBoardForm")
+	public String writeBoardForm() {
+		log.info("board.writeBoardForm() 실행");
+		return "board/writeBoardForm";
+	}
+
+	@PostMapping("/writeBoard")
+	public String writeBoard(BoardDto boardDto) {
+		if (boardDto.getBimg() != null && !boardDto.getBimg().isEmpty()) {
+			// DTO 추가 설정
+			try {
+				boardDto.setBimgdata(boardDto.getBimg().getBytes());
+			} catch (Exception e) {
+			}
+		}
+		boardDto.setMid("admin");
+		service.writeBoard(boardDto);
+
+		return "redirect:/board";
 	}
 }
