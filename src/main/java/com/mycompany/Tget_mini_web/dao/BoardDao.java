f1bc12d4e4efcd3e58cb25803a17de8412e3e1ad
김명환 <myeonghwan57@gmail.com>
2024-05-07 18:15:41 +0900

    명환 - BoardDao.java 파일 추가 작성(count,selectByPage)

--- src/main/java/com/mycompany/Tget_mini_web/dao/BoardDao.java
+++ src/main/java/com/mycompany/Tget_mini_web/dao/BoardDao.java
@@ -1,12 +1,19 @@
 package com.mycompany.Tget_mini_web.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.mycompany.Tget_mini_web.dto.BoardDto;
+import com.mycompany.Tget_mini_web.dto.PagerDto;
 
 @Mapper
 public interface BoardDao {
 
 	public int insert(BoardDto boardDto);
+
+	public int count();
+
+	public List<BoardDto> selectByPage(PagerDto pagerDto);
 
 }
