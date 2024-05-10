f751e3871308f5fc7429d38a3e0dfb48d13acdb8
김명환 <myeonghwan57@gmail.com>
2024-05-10 10:32:48 +0900

    명환: ItemController.java 파일 작성

--- src/main/java/com/mycompany/Tget_mini_web/controller/ItemController.java
+++ src/main/java/com/mycompany/Tget_mini_web/controller/ItemController.java
@@ -1,13 +1,23 @@
 package com.mycompany.Tget_mini_web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import com.mycompany.Tget_mini_web.dto.ReviewDto;
+
+import com.mycompany.Tget_mini_web.service.ReviewService;
 
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 @RequestMapping("/item")
 public class ItemController {
+	@Autowired
+	private ReviewService service;
+
 	@RequestMapping("")
 	public String detail() {
 		log.info("detail()실행");
@@ -19,6 +29,21 @@ public class ItemController {
 		log.info("item.status_page()실행");
 		return "item/status_page";
 	}
-
+	@RequestMapping("writeReviewForm")
+	public String writeReviewForm() {
+		log.info("item.writeReviewForm() 실행");
+		return "item/writeReviewForm";
+	}
 	
+	
+	@PostMapping("/writeReview")
+	public String writeReview(ReviewDto reviewDto) {
+		
+		reviewDto.setMid("admin");
+		reviewDto.setPno(20);
+		service.writeReview(reviewDto);
+
+		return "item/detail";
+	}
+		
 }
