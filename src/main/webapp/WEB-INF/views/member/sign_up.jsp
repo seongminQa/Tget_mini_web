732fabd6eab46d1aa22e609d3f06d38bd7c2cb5a
seongminQa <znznfltm@gmail.com>
2024-05-10 09:00:23 +0900

    성민 : 수민 sign_up.js // sign_up.jsp 업데이트

--- src/main/webapp/WEB-INF/views/member/sign_up.jsp
+++ src/main/webapp/WEB-INF/views/member/sign_up.jsp
@@ -81,60 +81,83 @@
         <form id="joinForm" name="joinForm" method="post" onsubmit="handleCheckData()" enctype="multipart/form-data" action="${pageContext.request.contextPath}/member/join" novalidate class="row d-flex flex-column align-items-center ms-5" style="margin-top: 50px; margin-bottom: 50px;">
             <div class="div_form row">
                 <label for="mid" class="col-3 pt-3"><strong>아이디</strong></label>
-                <input class="col-9" type="text" id="mid" name="mid" placeholder="6~20자 영문, 숫자">
+                <input class="col-7" type="text" id="mid" name="mid" placeholder="6~20자 영문 숫자 포함" oninput="idUniqueCheck()">
+                <button id="IdUniqueCheck" class="col-2 btn" type="button" style="border:none; background-color:#D95B96; color:white; height:40px; text-align: center;">중복체크 </button>
+            </div>
+            <div class="row" style="width: 40%;">
+                <span id="spanId" style="display: inline;" class="d-flex mt-3"></span>
             </div>
             <div class="div_form row">
                 <label for="mpassword" class="col-3 pt-3"><strong>비밀번호</strong></label>
                 <input class="pwClazz col-8" type="password" id="mpassword" name="mpassword" placeholder="8~12자 영문 대소문자 숫자 포함">
                 <div class="pwEye col-1"><button type="button" style="border:none; background-color: transparent;"><img src="/Tget_mini_web/resources/image/project_image/eye.svg" style="width: 30px;"></button></div>
             </div>
+            <div class="row" style="width: 40%;">
+                <span id="spanPw" style="display: inline;" class="d-flex mt-3"></span>
+            </div>
             <div class="div_form row">
                 <label for="pwcheck" class="col-3 pt-3"><strong>비밀번호 확인</strong></label>
                 <input class="pwClazzCheck col-8" type="password" id="pwcheck" name="pwcheck" placeholder="8~12자 영문 대소문자 숫자 포함">
                 <div class="pwEyeCheck col-1"><button type="button" style="border:none; background-color: transparent;"><img src="/Tget_mini_web/resources/image/project_image/eye.svg" style="width: 30px;"></button></div>
             </div>
+            <div class="row" style="width: 40%;">
+                <span id="spanPwCheck" style="display: inline;" class="d-flex mt-3"></span>
+            </div>
             <div class="div_form row">
                 <label for="mname" class="col-3 pt-3"><strong>이름</strong></label>
                 <input class="col-9" type="text" id="mname" name="mname" placeholder="ex. 홍길동">
+            </div>
+            <div class="row" style="width: 40%;">
+                <span id="spanName" style="display: inline;" class="d-flex mt-3"></span>
             </div>
             <div class="div_form row">
                 <label for="mssn" class="col-3 pt-3"><strong>주민등록번호</strong></label>
                 <input class="col-9" type="text" id="mssn" name="mssn" placeholder="하이픈을 포함해주세요" style="color:grey">
             </div>
+            <div class="row" style="width: 40%;">
+                <span id="spanSsn" style="display: inline;" class="d-flex mt-3"></span>
+            </div>
             <div class="div_form row">
                 <label for="memail" class="col-3 pt-3"><strong>이메일</strong></label>
                 <input class="col-9" type="text" id="memail" name="memail" placeholder="ex. tget0101@naver.com">
             </div>
-            <div class="div_form row">
+            <div class="row" style="width: 40%;">
+                <span id="spanEmail" style="display: inline;" class="d-flex mt-3"></span>
+            </div>
+   <!--          <div class="div_form row">
                 <label for="maddress" class="col-3 pt-3"><strong>주소</strong></label>
                 <input class="col-7" type="text" id="maddress" name="maddress" placeholder="ex. 서울특별시 종로구 대학로">
                 <div class="col-2"><a><button type="button" class="btn" style="background-color: #D95B96; color: white;">우편번호</button></a></div>
-            </div>
+            </div> -->
 <!--             <div class="div_form row">
                 <label for="mdetail_address" class="col-3 pt-3"><strong>상세 주소</strong></label>
                 <input class="col-9" type="text" id="mdetail_address" name="mdetail_address" placeholder="ex. 두산아트센터 연강홀 B1">
             </div> -->
-            <div class="div_form row"style="margin-bottom:15px">
+            <div class="div_form row">
                 <label for="mphone" class="col-3 pt-3"><strong>전화번호</strong></label>
                 <input class="col-9" type="text" id="mphone" name="mphone" placeholder="하이픈을 포함해주세요">
             </div>
+            <div class="row" style="width: 40%;">
+                <span id="spanPhone" style="display: inline;" class="d-flex mt-3"></span>
+            </div>
             <div class="div_form row">
-                <label for="mnickname" class="col-3 pt-3"><strong>닉네임</strong></label>
+                <label for="mnickname" class="col-3 pt-3"><strong>닉네임(선택)</strong></label>
                 <input class="col-9" type="text" id="mnickname" name="mnickname" placeholder="ex. 연뮤덕">
             </div>
-            
-
+            <div class="row" style="width: 40%;">
+                <span id="spanNickname" style="display: inline;" class="d-flex mt-3"></span>
+            </div>
             <div class="div_form row"style="margin-bottom:15px">
-                <label for="mprofileImg" class="col-3 pt-3"><strong>프로필 이미지</strong></label>
-                <input class="col-9"  type="file" id="mprofileImg" name="mprofileImg">
+                <label for="mprofileImg" class="col-5 pt-3"><strong>프로필 이미지(선택)</strong></label>
+                <input class="col-7"  type="file" id="mprofileImg" name="mprofileImg">
             </div>
             <div class="div_form_checkbox row">
                 <input class="col-1" type="checkbox" style="width: 20px;">
                 <span class="span_radio col-11" >SMS로 상품 및 이벤트 정보를 받겠습니다.(선택)</span>
             </div>
             <div class="div_form_checkbox row">
-                <input class="col-2" type="checkbox" style="width: 20px;">
-                <span class="span_radio col-10" >14세 미만입니다.</span>
+                <input id="necessaryBox" class="col-2" type="checkbox" style="width: 20px;">
+                <span id="necessaryBoxSpan" class="span_radio col-10" >14세 미만입니다.(필수)</span>
             </div>
             <div class="row" style="width: 40%; height: 60px;">
                 <span style="display: inline;" class="d-flex justify-content-center mt-3">만 14세 미만 회원은 법정 대리인(부모님) 동의를 받은 경우만 회원가입이 가능합니다.</span>
--- src/main/webapp/resources/js/sign_up.js
+++ src/main/webapp/resources/js/sign_up.js
@@ -1,129 +1,164 @@
 // 비밀번호 보이게 하는 스크립트
 
 $(function() { // html 문서 로드 이후 로드하는 스크립트
-	// 문서보다 먼저 로드 되면 해당 클래스를 인식할 수 없기에 문서로드 제이쿼리 안에 작성한다.
+   // 문서보다 먼저 로드 되면 해당 클래스를 인식할 수 없기에 문서로드 제이쿼리 안에 작성한다.
 
-	// 버튼을 누르면 비밀번호확인 보여주는 함수 실행
-	$(".pwEye").click(function() {
-		toggleView();
-	});
+   // 버튼을 누르면 비밀번호확인 보여주는 함수 실행
+   $(".pwEye").click(function() {
+      toggleView();
+   });
 
-	// 객체 찾아서 변수에 담기
-	const pwType = document.querySelector(".pwClazz");
-	const pwEye = document.querySelector(".pwEye");
+   // 객체 찾아서 변수에 담기
+   const pwType = document.querySelector(".pwClazz");
+   const pwEye = document.querySelector(".pwEye");
 
-	// input 타입 변환하는 함수
-	function toggleView() {
-		if (pwType.type === "password") {
-			pwType.type = "text";
-		} else {
-			pwType.type = "password";
-		}
-	}
+   // input 타입 변환하는 함수
+   function toggleView() {
+      if (pwType.type === "password") {
+         pwType.type = "text";
+      } else {
+         pwType.type = "password";
+      }
+   }
 });
 
 $(function() {
-	// 버튼을 누르면 비밀번호확인 보여주는 함수 실행
-	$(".pwEyeCheck").click(function() {
-		toggleView();
-	});
+   // 버튼을 누르면 비밀번호확인 보여주는 함수 실행
+   $(".pwEyeCheck").click(function() {
+      toggleView();
+   });
 
-	const pwType = document.querySelector(".pwClazzCheck");
-	const pwEye = document.querySelector(".pwEyeCheck");
+   const pwType = document.querySelector(".pwClazzCheck");
+   const pwEye = document.querySelector(".pwEyeCheck");
 
-	function toggleView() {
-		if (pwType.type === "password") {
-			pwType.type = "text";
-		} else {
-			pwType.type = "password";
-		}
-	}
+   function toggleView() {
+      if (pwType.type === "password") {
+         pwType.type = "text";
+      } else {
+         pwType.type = "password";
+      }
+   }
 });
+
+////////////////////////////////////////////////////////////////
+// id 중복 검사(
+function idUniqueCheck(){
+   console.log("idUniqueCheck 실행");
+
+   const mid = $('#mid').val();
+   $.ajax({
+       url: 'uniqueid',
+       type: 'post',
+       data: {mid:mid},
+       success: function(data){
+          console.log("json객체의 count값"+data.count);
+           if(data.count == 0){
+               // 중복값이 없다면
+               $("#spanId").removeClass("text-danger").text("사용 가능한 아이디 입니다.");
+           } else {
+               $("#spanId").addClass("text-danger").text("이미 있는 아이디 입니다.");
+               
+           }
+       }
+   });
+};
 
 //////////////////////////////////////////////////////////////////////////////////////
 // form 유효성 검사
 
 function handleCheckData() {
-	console.log("입력 데이터를 검사합니다.");
-	
-	//form 태그의 action 기능을 수행하지 않도록 함
-	event.preventDefault();
-	
-	//각 입력 양식의 데이터 검사
-	var totalResult = true;
-	
-	//1)ID 검사 -------------------------------------------------				
-	var midPattern = /^(?=.*\d)(?=.*[a-z]).{6,20}$/;
-	var midResult = midPattern.test($("#mid").val());
-	if(midResult) {
-		$("#mid").removeClass("border border-danger");
-	} else {	
-		$("#mid").addClass("border border-danger");
-		totalResult = false;
-	}
+   console.log("입력 데이터를 검사합니다.");
+   
+   //form 태그의 action 기능을 수행하지 않도록 함
+   event.preventDefault();
+   
+   //각 입력 양식의 데이터 검사
+   var totalResult = true;
+   
+   //1)ID 검사 -------------------------------------------------            
+   var midPattern = /^(?=.*\d)(?=.*[a-z]).{6,20}$/;
+   var midResult = midPattern.test($("#mid").val());
+   if(midResult) {
+      $("spanId").removeClass("text-danger").html("");
+   } else {   
+      $("#spanId").addClass("text-danger").html("아이디가 양식에 맞지 않습니다.");
+      totalResult = false;
+   }
 
     //2)Password 검사 -------------------------------------------------
-	var mpasswordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$/;
-	var mpasswordResult = mpasswordPattern.test($("#mpassword").val());
-	if(mpasswordResult) {
-		$("#mpassword").removeClass("border border-danger");
-	} else {	
-		$("#mpassword").addClass("border border-danger");
-		totalResult = false;
-	}
+   var mpasswordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$/;
+   var mpasswordResult = mpasswordPattern.test($("#mpassword").val());
+   if(mpasswordResult) {
+      $("#spanPw").removeClass("border border-danger");
+   } else {   
+      $("#spanPw").addClass("text-danger").html("비밀번호가 양식에 맞지 않습니다.");
+      totalResult = false;
+   }
 
     //2-1)pwcheck 검사 -------------------------------------------------
-	if($("#mpassword").val()===$("#pwcheck").val()) {
-		$("#pwcheck").removeClass("border border-danger");
-	} else {	
-		$("#pwcheck").addClass("border border-danger");
-		totalResult = false;
-	}
+   if($("#mpassword").val()===$("#pwcheck").val()) {
+      $("#spanPwCheck").removeClass("border border-danger");
+   } else {   
+      $("#spanPwCheck").addClass("text-danger").html("비밀번호가 일치하지 않습니다.");
+      totalResult = false;
+   }
 
     //3)이름 검사 -------------------------------------------------
-	var mnamePattern = /^[가-힣]{2,12}$/;
-	var mnameResult = mnamePattern.test($("#mname").val());
-	if(mnameResult) {
-		$("#mname").removeClass("border border-danger");
-	} else {	
-		$("#mname").addClass("border border-danger");
-		totalResult = false;
-	}
+   var mnamePattern = /^[가-힣]{2,12}$/;
+   var mnameResult = mnamePattern.test($("#mname").val());
+   if(mnameResult) {
+      $("#spanName").removeClass("border border-danger");
+   } else {   
+      $("#spanName").addClass("text-danger").html("이름이 양식에 맞지 않습니다.");
+      totalResult = false;
+   }
 
     //4)주민등록번호 검사 -------------------------------------------------
-	var mssnPattern = /^\d{6}\-\d{7}$/;
-	var mssnResult = mssnPattern.test($("#mssn").val());
-	if(mssnResult) {
-		$("#mssn").removeClass("border border-danger");
-	} else {	
-		$("#mssn").addClass("border border-danger");
-		totalResult = false;
-	}
-	
-	//5)Email 검사 -------------------------------------------------	
-	var memailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	var memailResult = memailPattern.test($("#memail").val());
-	if(memailResult) {
-		$("#memail").removeClass("border border-danger");
-	} else {	
-		$("#memail").addClass("border border-danger");
-		totalResult = false;
-	}
-	
+   var mssnPattern = /^\d{6}\-\d{7}$/;
+   var mssnResult = mssnPattern.test($("#mssn").val());
+   if(mssnResult) {
+      $("#spanSsn").removeClass("border border-danger");
+   } else {   
+      $("#spanSsn").addClass("text-danger").html("주민등록번호가 양식에 맞지 않습니다.");
+      totalResult = false;
+   }
+   
+   //5)Email 검사 -------------------------------------------------   
+   var memailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+   var memailResult = memailPattern.test($("#memail").val());
+   if(memailResult) {
+      $("#spanEmail").removeClass("border border-danger");
+   } else {   
+      $("#spanEmail").addClass("text-danger").html("이메일이 양식에 맞지 않습니다.");
+      totalResult = false;
+   }
+   
 
-	//6)Phone 검사 ---------------------------------------------------
-	var mphonePattern = /^010-\d{3,4}-\d{4}$/;
-	var mphoneResult = mphonePattern.test($("#mphone").val());
-	if(mphoneResult) {
-		$("#mphone").removeClass("border border-danger");
-	} else {	
-		$("#mphone").addClass("border border-danger");
-		totalResult = false;
-	}
-	
-	//7) 전체 유효성 검사 결과가 true일 경우
-	if(totalResult) {
-		//수동으로 action 기능을 수행하도록 함
-		$("#joinForm")[0].submit();
-	}
+   //6)Phone 검사 ---------------------------------------------------
+   var mphonePattern = /^010-\d{3,4}-\d{4}$/;
+   var mphoneResult = mphonePattern.test($("#mphone").val());
+   if(mphoneResult) {
+      $("#spanPhone").removeClass("border border-danger");
+   } else {   
+      $("#spanPhone").addClass("text-danger").html("휴대폰 번호가 양식에 맞지 않습니다.");
+      totalResult = false;
+   }
+   
+   //7)체크박스 검사 -------------------------------------------------
+   if ($('#necessaryBox').is(':checked')) {
+       // 체크되어 있음
+       //console.log('체크되어 있음');
+       $("#necessaryBoxSpan").removeClass("text-danger");
+   } else {
+       // 체크되어 있지 않음
+      totalResult = false;
+       //console.log('체크되어 있지 않음');
+       $("#necessaryBoxSpan").addClass("text-danger");
+   }
+   
+   //8)전체 유효성 검사 결과가 true일 경우
+   if(totalResult) {
+      //수동으로 action 기능을 수행하도록 함
+      $("#joinForm")[0].submit();
+   }
 }
