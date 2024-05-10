67733a4efb6e65971481dab450340f9149dd0ec7
김명환 <myeonghwan57@gmail.com>
2024-05-10 10:31:58 +0900

    명환: ReviewDao.java 파일 작성

--- src/main/java/com/mycompany/Tget_mini_web/dao/ReviewDao.java
+++ src/main/java/com/mycompany/Tget_mini_web/dao/ReviewDao.java
@@ -2,7 +2,13 @@ package com.mycompany.Tget_mini_web.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.mycompany.Tget_mini_web.dto.ReviewDto;
+
 @Mapper
 public interface ReviewDao {
+
+	int insert(ReviewDto reviewDto);
+
+	
 
 }
