255ce433975b5d811b4f47f0ee8fde38f3973c97
seongminQa <znznfltm@gmail.com>
2024-05-16 21:25:41 +0900

    성민 : productcontroller.java 수정 (명환)

--- src/main/java/com/mycompany/Tget_mini_web/controller/ProductController.java
+++ src/main/java/com/mycompany/Tget_mini_web/controller/ProductController.java
@@ -1,27 +1,25 @@
 package com.mycompany.Tget_mini_web.controller;
 
 import java.io.OutputStream;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mycompany.Tget_mini_web.dao.CartDao;
 import com.mycompany.Tget_mini_web.dao.ProductDao;
 import com.mycompany.Tget_mini_web.dto.CartDto;
 import com.mycompany.Tget_mini_web.dto.MemberDto;
-import com.mycompany.Tget_mini_web.dto.PagerDto;
 import com.mycompany.Tget_mini_web.dto.ProductDto;
 import com.mycompany.Tget_mini_web.security.TgetUserDetails;
 import com.mycompany.Tget_mini_web.service.CartService;
@@ -41,12 +39,12 @@ public class ProductController {
 	
 	// 상품 리스트
 	@RequestMapping("")
-		public String productList(Model model) {
+	public String productList(Model model) {
 		 
 		// 페이징 정보를 얻어서 서비스쪽으로 넘기고  Service에서 게시물 목록 요청
 		List<ProductDto> productList = productService.getShoppingProductList();
 		model.addAttribute("productList", productList);
-		  
+		
 		return "/item/productList";
 	}
 	
@@ -68,6 +66,25 @@ public class ProductController {
       os.flush();
       os.close();
    }
+   
+   @GetMapping("/attachProductContent")
+   public void attachProductContent(int pno, HttpServletResponse response) throws Exception {
+      // 다운로드할 데이터를 준비
+      ProductDto productDto = productService.getProduct(pno); // 선택한 pno에 해당하는 인스턴스의 정보를 가져와 productDto 필드에 저장
+      byte[] data = productService.getAttachProductContent(pno);// 선택한 pno에 해당하는 포스터 데이터를 가져와 byte[] 형식으로 저장
+      
+      // 응답 헤더 구성
+      // 콘텐츠의 타입을 응답컨텐츠 타입으로 저장
+      response.setContentType(productDto.getPcontentattachtype());
+      response.setContentType(productDto.getPposterattachtype());
+      
+      // 응답 본문에 파일 데이터 출력
+      OutputStream os = response.getOutputStream();
+      os.write(data);
+      os.flush();
+      os.close();
+   }
+
    
    // 상품 상세 페이지
    @GetMapping("/detail")
@@ -105,78 +122,75 @@ public class ProductController {
    // 장바구니 리스트(cart)
    //@Secured("ROLE_USER")
    @RequestMapping("/cart")
-   public String cart(HttpSession session, Model model, String pageNo,
+   public String cart(HttpSession session, Model model, String pageNo, 
 		   Authentication authentication) {
 	   
-	   // pageNo를 받지 못했을 경우
-       if(pageNo == null) {
-          pageNo = (String) session.getAttribute("pageNo");
-          // 세션에 저장되어 있지 않을 경우 "1"로 강제 세팅
-          if(pageNo == null) {
-             pageNo = "1";
-          }
-       }
+	   log.info("cart 컨트롤러 실행");
       
-       // 세션에 pageNo 저장
-       session.setAttribute("pageNo", pageNo);
-       
-       // 문자열을 정수로 변환
-       int intPageNo = Integer.parseInt(pageNo);
-       
-       // Pager 객체 생성
-       // 사용자가 제일 처음에는 페이지의 번호를 입력하지 않는다. --> 사용자에 의해 요청한 페이지가 아닌 것을 보여주어야 한다(초기화면)
-       int rowsPagSingTarget = cartService.getTotalRows();
-       PagerDto pager = new PagerDto(5, 5, rowsPagSingTarget, intPageNo); // 페이지 객체 만들기
-      
-       // 페이징 정보를 얻어서 서비스쪽으로 넘기고  Service에서 카트 디테일 목록 요청
+       // 현재 로그인한 회원의 정보 얻기
        TgetUserDetails userDetails = (TgetUserDetails) authentication.getPrincipal();
        MemberDto memberDto = userDetails.getMember();
-       model.addAttribute("memberDto", memberDto);
-       
-       //List<CartDetailDto> cart = cartService.getCartList(pager);
-       
-       // 현재 로그인한 Mid 콘솔로 확인하기
-       log.info("authentication 테스트 : " + memberDto.getMid());
-       
-       // jsp에서 사용할 수 있도록 설정.
-       model.addAttribute("pager", pager);
-       //model.addAttribute("currentMid", currentMid);
-       //log.info(currentMid);
-       //model.addAttribute("cartList", cart);
-       
+	   
+       List<CartDto> cartList = cartService.getCartItemList(memberDto.getMid());
+       // 카트리스트에 넘기기전에 상품금액을 갱신하여 전달 (원래 상품의 가격 * 수량)
+       for(CartDto cartDto : cartList) {
+    	   cartDto.setResultprice(cartDto.getPprice()*cartDto.getOamount());
+       }
+       List<ProductDto> productList = productService.getShoppingProductList();
+       model.addAttribute("productList", productList);
+       model.addAttribute("cartList", cartList);
        
        return "/member/shopping_cart";
 	}
 	
-   
-//   @RequestMapping(value="/addCartItem/${pno}", method=RequestMethod.GET)
+   // 상품 상세페이지에서 '장바구니 추가' 버튼을 누르면 실행
    @PostMapping("/addCartItem")
-   /*public String addCartItem(@RequestParam(defaultValue="0") Integer cno, int pno, int oamount, String oseatgrade, @DateTimeFormat(pattern="yyyy-MM-dd") Date odate,
-		   Authentication authentication) {*/
-   public String addCartItem(CartDto cartDto,
-		   Authentication authentication) {
-
+   public String addCartItem(CartDto cartDto, Authentication authentication) {
+	  
+	  log.info("addCartItem 컨트롤러 실행");
 	  TgetUserDetails userDetails = (TgetUserDetails) authentication.getPrincipal();
 	  MemberDto memberDto = userDetails.getMember();
-	  String mid = memberDto.getMid();
 
 	  cartDto.setMid(memberDto.getMid());
-	  cartService.addCartItem(cartDto);
 	  
-      return "/member/shopping_cart";
+	  log.info(cartDto.getOdate().toString());
+	  log.info(cartDto.getOseatgrade());
+	  
+	  // 만약 해당 상품이 장바구니에 추가 되어있다면, (옵션 : 좌석의 등급과 날짜가 같다면)
+	  // 해당하는 장바구니 품목에 수량을 변경해준다.
+	  List<CartDto> cartList = cartService.getCartItemList(memberDto.getMid());
+
+	  boolean isUpdate = false;
+	  for(CartDto item : cartList) {
+		 log.info("for문의 item, CartDto 확인1 : " + item.getOseatgrade().toString() + cartDto.getOseatgrade().toString());
+		 log.info("for문의 item, CartDto 확인1 : " + item.getOdate().toString() + cartDto.getOdate().toString());
+		 if(item.getOseatgrade() == cartDto.getOseatgrade()
+				 && item.getOdate() == cartDto.getOdate()) {
+			 item.setOamount(cartDto.getOamount() + item.getOamount());
+			 cartService.updateCartItem(item);
+			 isUpdate = true;
+			 break;
+		 }
+	  }
+	  
+	  if(!isUpdate) {
+		  cartService.addCartItem(cartDto);
+	  }
+
+      return "redirect:/product/cart";
    }
 	
-	// 업데이트 방법 2  // get을 사용한 경우에는 이렇게 가능하다. set을 사용한 경우는 못함!
-	/*@RequestMapping(value="/updateCartItem", produces="application/json; charset=UTF-8")
+	// 업데이트 방법   // get을 사용한 경우에는 이렇게 가능하다. set을 사용한 경우는 못함!
+	@RequestMapping(value="/updateCartItem", produces="application/json; charset=UTF-8")
 	@ResponseBody
-	public String updateCartItem(int pno, int amount, @SessionAttribute("cart") List<Ch08CartItem> cart) {
+	public String updateCartItem(int pno, int amount, List<CartDto> cart) {
 		// 카트안의 pno를 찾고 그 pno의 수량(amount)를 찾는다? 적용시킨다?
 		
 		// pno와 같은 CartItem 찾기
-		for(Ch08CartItem item : cart) {
-			if(item.getProduct().getPno() == pno) {
+		for(CartDto item : cart) {
+			if(item.getPno() == pno) {
 				// CartItem의 amount를 수정
-				item.setAmount(amount); // 세션의 정보를 set하는게 아니라 cart의 아이템을 set하는거라 쓸 수 있음
+				item.setOamount(amount); // 세션의 정보를 set하는게 아니라 cart의 아이템을 set하는거라 쓸 수 있음
 			}
 		}
 		
@@ -185,23 +199,16 @@ public class ProductController {
 		jsonObject.put("result", "success");
 		
 		return jsonObject.toString();
-	}*/
+	}
 	
-	// 제거 방법 2 // get을 사용한 경우에는 이렇게 가능하다. set을 사용한 경우는 못함!
-	/*@RequestMapping("/removeCartItem")
-	public String removeCartItem(int pno, @SessionAttribute("cart") List<Ch08CartItem> cart) {
-
-		// 위의 방식처럼 for문을 돌리면 지웠는데, 길이는 남아있어서 에러가 난다.
-		// 따라서 이터레이터를 사용해야한다.
-		Iterator<Ch08CartItem> iterator = cart.iterator();
-		while(iterator.hasNext()) {
-			Ch08CartItem cartItem = iterator.next();
-			if(cartItem.getProduct().getPno() == pno) {
-				log.info(pno + "삭제완료");
-				iterator.remove();
-			}
-		}
-		return "redirect:/ch08/cartView";
-	}*/
+	// 장바구니 하나의 아이템 제거 (삭제)
+	// cart 데이터테이블에서 하나의 인스턴스 삭제 (cno로 구분)
+	@RequestMapping("/removeCartItem")
+	public String removeCartItem(int cno) {
+		log.info("removeCartItem 실행");
+		// Cart 테이블에서 해당 인스턴스 삭제
+		cartService.removeCartItem(cno);
+		return "redirect:/product/cart";
+	}
    
 }
